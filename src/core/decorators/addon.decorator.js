3afadfdff172df0a6ae55f7c2f6ac350a35939c9
Mateusz Szmytko <mateusz.szmytko@gmail.com>
2017-06-13 02:32:36 +0200

    cleaning

--- src/core/decorators/addon.decorator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function _Addon(options) {
-    return function (constructor) {
-    };
-}
-exports._Addon = _Addon;
-//# sourceMappingURL=addon.decorator.js.map
--- src/core/init.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function Init(app) {
-    var _app = new (app.bind.apply(app, [void 0].concat(app.prototype.modules)))();
-    _app.__appInit();
-}
-exports.Init = Init;
-//# sourceMappingURL=init.js.map
